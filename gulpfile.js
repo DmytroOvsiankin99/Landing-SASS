c7df3102221bb667a0a36b69e75a286b1d3f4284
DmitriyDDS <dmitriy.shelby@gmail.com>
2019-05-30 01:53:51 +0300

    first load

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+const gulp = require('gulp');//npm install --save-dev gulp
+const rename = require('gulp-rename');
+const sass = require('gulp-sass');
+const  autoprefixer = require ('gulp-autoprefixer');
+const  sourcemaps = require ('gulp-sourcemaps');
+const  browser_sync = require ('browser-sync').create();//npm install --save-dev browser-sync
+
+function scss_css(done){
+    gulp.src('./sass/**/*.scss')
+    .pipe( sourcemaps.init())
+    .pipe( sass({
+        errorLogToConsole: true, 
+        outputStyle: "compressed"
+    }))
+    .on('error', console.error.bind(console))
+    .pipe( autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false
+    }))
+    .pipe( rename({suffix:'.min'}))
+    .pipe(sourcemaps.write('./'))
+    .pipe( gulp.dest('./css/'))
+    .pipe(browser_sync.stream());
+    done();
+}
+function sync(done){
+    browser_sync.init({
+        server: {
+            baseDir: "./"
+        },
+        port: 3000
+    });
+    done();
+}
+function reload(done){
+    browser_sync.reload();
+    done();
+}
+function watchFiles(){
+    gulp.watch("./sass/**/*", scss_css);
+    gulp.watch("./**/*.html", reload);
+    gulp.watch("./**/*.js", reload);
+    gulp.watch("./**/*.php", reload);
+}
+gulp.task('default', gulp.parallel(watchFiles, sync));
